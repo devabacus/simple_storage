fddd58c94e93fb799d8ae7ed886270d5bea588a9
mrfrolk <ekaterita20@gmail.com>
2025-03-22 06:52:14 +0500

    1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,36 +1,39 @@
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:simple_storage/simple_storage.dart';
 import 'package:simple_storage/simple_storage_interface.dart';
 
-void main(List<String> args) {
-  final userStorage = UserStorageService(storageRepo: SharedPrefRepo());
-  
+void main(List<String> args) async {
+  final userStorage = await getStorageService();
   userStorage.getName().then(print);
-
   userStorage.getAge().then(print);
+  userStorage.saveName("Ivan");
+  userStorage.saveAge(25);
+}
 
-  userStorage.saveName("Stas");
-  userStorage.saveAge(40);
-
+Future<UserStorageService> getStorageService() async {
+  final sPref = await SharedPreferences.getInstance();
+  final sPrefRepo = SharedPrefRepo(pref: sPref);
+  return UserStorageService(storageRepo: sPrefRepo);
 }
 
 class UserStorageService {
-  final SimpleStorageInterface? storageRepo;
+  final SimpleStorageInterface storageRepo;
 
   UserStorageService({required this.storageRepo});
 
   void saveName(String name) {
-    storageRepo?.saveString(key: 'userName', val: name);
+    storageRepo.saveString(key: 'userName', val: name);
   }
 
   void saveAge(int age) {
-    storageRepo?.saveInt(key: 'userAge', val: age);
+    storageRepo.saveInt(key: 'userAge', val: age);
   }
 
-  Future<String?> getName() async {
-    return storageRepo?.getString(key: 'userName');
+  Future<String> getName() async {
+    return storageRepo.getString(key: 'userName');
   }
 
-  Future<int?> getAge() async {
-    return storageRepo?.getInt(key: 'userAge');
+  Future<int> getAge() async {
+    return storageRepo.getInt(key: 'userAge');
   }
 }
